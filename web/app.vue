7552f742b2775f70d64df8f674e10ec138687f71
Eduardo Castillo <eduardocastilloh1999@gmail.com>
2024-11-02 21:53:49 -0600

    Initial commit

---
+++ web/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <NuxtRouteAnnouncer />
+
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
